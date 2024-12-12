60c5615db4c8f845fe133653ce562a908d3888d0
mik-305 <mik-305@bk.ru>
2024-12-12 14:32:25 +0300

    Update module_os_3.py

--- module_os_3.py
+++ module_os_3.py
@@ -7,11 +7,11 @@ directory = 'c:\\intel\\Logs\\'                                     # Дирек
 
 for root, dirs, files in os.walk(directory):
     for file in files:
-        filepath = os.path.join(directory)                          # Путь к файлу
-        filetime = os.path.getmtime(os.path.join(directory) + file)
+        filepath = os.path.join(root, file)                         # Путь к файлу
+        filetime = os.path.getmtime(filepath)
         formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime)) # Дата и время изменения файла
-        filesize = os.path.getsize(os.path.join(directory) + file)  # Размер файла
-        parent_dir = os.path.dirname(root)                          # Родительская директория
+        filesize = os.path.getsize(filepath)                        # Размер файла
+        parent_dir = os.path.dirname(filepath)                      # Родительская директория
         print(f'Обнаружен файл: {file}, Путь: {filepath}, Размер: {filesize} байт, Время изменения: {formatted_time}, '
         f'Родительская директория: {parent_dir}')
 
