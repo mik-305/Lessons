68a44a26fe6bc95b236065c944e3482089cc1c85
mik-305 <mik-305@bk.ru>
2024-12-17 09:41:43 +0300

    Update module_7_3.py

--- module_7_3.py
+++ module_7_3.py
@@ -1,55 +1,48 @@
+import string
 class WordsFinder:
-
-    all_words = {}  # Создание пустого словаря
     def __init__(self, *args):
         self.file_names = args
     def get_all_words(self):
-        i = 0                               # Счётчик количества входных файлов
-        with open(self.file_names[i], encoding='utf-8') as file:
-            global line
-            line = file.read()              # Чтение из файла
-            line = line.lower()             # Перевод в нижнйи регистр
-            line = line.replace(',', ' ')
-            line = line.replace('!', ' ')
-            line = line.replace('=', ' ')
-            line = line.replace('.', ' ')
-            line = line.replace('?', ' ')
-            line = line.replace(';', ' ')
-            line = line.replace(':', ' ')
-            line = line.replace('-', ' ')
-            line = line.replace('\n', '')
-            line = line.split(sep=' ')
-        all_words = {self.file_names[0]: line}      # Заносим данные в словарь
+        trans = {',':' ','.':' ','!':' ','?':' ',';':' ','-':' '} # Таблица первода символов
+        all_words = {}                                            # Создание пустого словаря
+        i = 0                                                     # Счётчик количества входных файлов
+        global file_name
+        for file_name in self.file_names:
+            #print(file_name)
+            #print('')
+            with open(self.file_names[i], encoding='utf-8') as file: # Открываем файл
+                i += 1
+                line = file.read().lower()                                   # Чтение из файла
+                #line = line.lower()
+                line = line.replace('\n', '')
+                tbl = line.maketrans(trans)
+                line = line.translate(tbl)            # Убираем заданые символы(по таблице трансляции)
+                line = line.split(sep=' ')
+            all_words = {self.file_names[i-1]: line}  # Заносим данные в словарь
+            #print(all_words)
         return all_words
-
     def find(self, word):
-        nom_in_str = 0                              # Номер искомого слова
-        line_str = str(line)
-        words = line_str.split(' ')
-        for j in range(len(words)):
-            if words[j].find(word.lower()) != -1:
-                nom_in_str = j+1
-                break
-        all_words = {self.file_names[0]: nom_in_str}
+        word = word.lower()
+        for name, words  in self.get_all_words().items():
+            j = 0  # Счётчик позиции искомого слова
+            for j in range(len(words)):
+                if word == words[j]:
+                    break
+            all_words = {self.file_names[0]: j + 1}
         return all_words
-
     def count(self, word):
-        line_str = str(line)
-        kol_sovp = 0                        # Количество нахождений искомого слова
-        words = line_str.split(' ')
-        for j in range(len(words)):
-            if word.lower() in words[j].lower():
-                kol_sovp +=1
-        all_words = {self.file_names[0]: kol_sovp}
+        word = word.lower()
+        for name, words in self.get_all_words().items():
+            j = 0
+            kol_vh = 0                       # Количество вхождений искомого слова
+            for j in range(len(words)):
+                if word == words[j]:
+                    kol_vh +=1
+            all_words = {self.file_names[0]: kol_vh}
         return all_words
-
-
-
-
 
 finder2 = WordsFinder('test_file.txt')
 print(finder2.get_all_words()) # Все слова
 print(finder2.find('TEXT')) # 3 слово по счёту
 print(finder2.count('teXT')) # 4 слова teXT в тексте всего
-
 
