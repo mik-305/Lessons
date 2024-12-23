af0aa980e7cd9659356d8eeab3f9ab4de484080e
mik-305 <mik-305@bk.ru>
2024-12-23 10:07:18 +0300

    Update module_13_1.py

--- module_13_1.py
+++ module_13_1.py
@@ -1,29 +1,27 @@
-#import time
 import asyncio
-name = ''
-power = 0
+
+async def start_strongman(name: str, power: int):  # Асинхронная функция для имитации соревнований
+    print(f'Силач {name} начал соревнования.')
+    print('')
+    for i in range(1, 6):  # Каждый силач поднимает 5 шаров
+        await asyncio.sleep(1 / power)          # Задержка зависит от силы
+        print(f'Силач {name} поднял шар № {i}')
+    print(f'Силач {name} закончил соревнования.')
+    
+async def start_tournament():                   # Асинхронная функция для проведения соревнований
+
+    atlets = [                          # Список наших участников соревнований
+        ('Паша', 3),
+        ('Денис', 4),
+        ('Аполлон', 5)
+    ]
+
+
+    tasks = [asyncio.create_task(start_strongman(name, power)) for name, power in atlets] # Каждый атлет выступает
+
+
+    await asyncio.gather(*tasks)        # Ожидание завершения выступления всех атлетов
 
 
 
-async def start_strongman(name, power):
-    for i in range(5):                              # Количество шаров
-        print(f'Силач {name} поднял шар № {i+1}')
-
-async def  start_tournament(name, power):
-    await asyncio.sleep(1/power)
-    print(f'Силач {name} закончил соревнования')
-
-
-async def main(name, power):
-    #task = asyncio.create_task(start_strongman(name, power))
-    print(f'Силач {name} начал соревнования')
-    task_1 = asyncio.create_task(start_strongman(name, power))
-    task_2 = asyncio.create_task(start_tournament(name, power))
-    await task_1
-    await task_2
-
-
-asyncio.run(main('Паша', 3 ))
-asyncio.run(main('Денис', 4 ))
-asyncio.run(main('Apollon', 5 ))
-#result = main()
+asyncio.run(start_tournament())         # Начинаем соревнование
